29bd13b9c884a4e8b7e28a71c9d8a0a108786b0d
unknown <chuslandaburu@telefonica.net>
2021-11-30 09:38:51 +0100

    primera copia

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Regex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string frase = "Encantado de veros 2344 reunidos";
+            string patron = "(cans)";
+
+            Regex miRegex = new Regex(patron);
+            MatchCollection vale = miRegex.Matches(frase);
+            Console.WriteLine(vale.Count >=1? "Se ha encontrado" : "De eso nada");
+            Console.WriteLine(miRegex.Matches(frase).Count >= 1 ? "Se ha encontrado" : "De eso nada");
+            
+
+        }
+    }
+}
